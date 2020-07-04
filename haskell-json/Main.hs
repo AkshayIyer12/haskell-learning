d24dd5ea9971bf36d19c6f726df9575d93bb9be3
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2020-07-04 08:47:41 +0530

    Add boolean parser

--- haskell-json/Main.hs
+++ haskell-json/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char
+import Control.Applicative
 
 data JsonValue
   = JsonNull
@@ -21,6 +22,18 @@ instance Functor Parser where
                           (input', x) <- p input
                           Just (input', f x)
 
+
+instance Applicative Parser where
+    pure x = Parser $ \input -> Just (input, x)
+    (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+                                    (input', f) <- p1 input
+                                    (input'', a) <- p2 input'
+                                    Just(input'', f a)
+
+instance Alternative Parser where
+    empty = Parser $ \_ -> Nothing
+    (Parser p1) <|> (Parser p2) = Parser $ \input ->
+                                     p1 input <|> p2 input
 -- *Main> runParser (charP 'n') "nice"
 -- Just ("ice",'n')
 -- *Main> runParser (charP 'n') "Hello"
@@ -34,11 +47,17 @@ charP x = Parser f
           | otherwise = Nothing
       f [] = Nothing
 
--- stringP :: String -> Parser String
--- stringP = sequenceA . map charP
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
 
 jsonNull :: Parser JsonValue
-jsonNull = undefined
+jsonNull = (\_ -> JsonNull) <$> stringP "null"
+
+jsonBool :: Parser JsonValue
+jsonBool = f <$> (stringP "true" <|> stringP "false")
+    where f "true"  = JsonBool True
+          f "false" = JsonBool False
+          f _       = undefined
 
 jsonValue :: Parser JsonValue
 jsonValue = undefined
