4d0f847b65b95f6b0a69ef7ec13466dbf2cc7a8c
Akshayiyer12 <c.m.akshay.iyer@gmail.com>
2020-07-21 17:24:27 +0530

    Add number parser

--- haskell-json/Main.hs
+++ haskell-json/Main.hs
@@ -6,61 +6,77 @@ import Control.Applicative
 data JsonValue
   = JsonNull
   | JsonBool Bool
-  | JsonNumber Integer
+  | JsonNumber Integer -- no support for floats
   | JsonString String
   | JsonArray [JsonValue]
   | JsonObject [(String, JsonValue)]
   deriving (Show, Eq)
 
---Note: no proper error reporting
+-- no proper error reporting
 newtype Parser a = Parser
   { runParser :: String -> Maybe (String, a)
   }
 
 instance Functor Parser where
-    fmap f (Parser p) = Parser $ \input -> do
-                          (input', x) <- p input
-                          Just (input', f x)
-
+  fmap f (Parser p) =
+    Parser $ \input -> do
+      (input', x) <- p input
+      Just (input', f x)
 
 instance Applicative Parser where
-    pure x = Parser $ \input -> Just (input, x)
-    (Parser p1) <*> (Parser p2) = Parser $ \input -> do
-                                    (input', f) <- p1 input
-                                    (input'', a) <- p2 input'
-                                    Just(input'', f a)
+  pure x = Parser $ \input -> Just (input, x)
+  (Parser p1) <*> (Parser p2) =
+    Parser $ \input -> do
+      (input', f) <- p1 input
+      (input'', a) <- p2 input'
+      Just (input'', f a)
 
 instance Alternative Parser where
-    empty = Parser $ \_ -> Nothing
-    (Parser p1) <|> (Parser p2) = Parser $ \input ->
-                                     p1 input <|> p2 input
--- *Main> runParser (charP 'n') "nice"
--- Just ("ice",'n')
--- *Main> runParser (charP 'n') "Hello"
--- Nothing
-
-charP :: Char -> Parser Char
-charP x = Parser f
-    where
-      f (y:ys)
-          | y == x    = Just (ys, x)
-          | otherwise = Nothing
-      f [] = Nothing
-
-stringP :: String -> Parser String
-stringP = sequenceA . map charP
+  empty = Parser $ \_ -> Nothing
+  (Parser p1) <|> (Parser p2) =
+      Parser $ \input -> p1 input <|> p2 input
 
 jsonNull :: Parser JsonValue
 jsonNull = (\_ -> JsonNull) <$> stringP "null"
 
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f (y:ys)
+      | y == x = Just (ys, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
+
 jsonBool :: Parser JsonValue
 jsonBool = f <$> (stringP "true" <|> stringP "false")
-    where f "true"  = JsonBool True
-          f "false" = JsonBool False
-          f _       = undefined
+   where f "true"  = JsonBool True
+         f "false" = JsonBool False
+         -- This should never happen
+         f _       = undefined
+
+spanP :: (Char -> Bool) -> Parser String
+spanP f =
+  Parser $ \input ->
+    let (token, rest) = span f input
+     in Just (rest, token)
+
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) =
+  Parser $ \input -> do
+    (input', xs) <- p input
+    if null xs
+      then Nothing
+      else Just (input', xs)
+
+jsonNumber :: Parser JsonValue
+jsonNumber = f <$> notNull (spanP isDigit)
+    where f ds = JsonNumber $ read ds
 
 jsonValue :: Parser JsonValue
-jsonValue = undefined
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber
 
 main :: IO ()
 main = undefined
